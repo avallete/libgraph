681f2f4f3cb5f5a9e5e54e86d807668970d2a009
Andrew Valleteau <avallete@student.42.fr>
2017-05-18 18:10:59 +0200

    [WIP]

--- main.c
+++ main.c
@@ -101,22 +101,66 @@ void		print_matrix(t_mat_graph *graph)
 	}
 }
 
+#define CHECK_BIT(var, pos) ((var) & (1 << (pos)))
+
+int has_link(t_mat_graph *graph, unsigned int src, unsigned int dst)
+{
+	unsigned char valmod;
+	int byte;
+
+	valmod = (dst % CHAR_BIT);
+	if (valmod)
+	{
+		byte = graph->matrix[src][dst / CHAR_BIT];
+		printf("byteindex1: %d\n", (dst / CHAR_BIT));
+	}
+	else if ((dst / CHAR_BIT) == 0)
+	{
+		byte = graph->matrix[src][(dst / CHAR_BIT)];
+		printf("byteindex2: %d\n", ((dst / CHAR_BIT)));
+	}
+	else
+	{
+		byte = graph->matrix[src][((dst / CHAR_BIT) - 1)];
+		printf("byteindex3: %d\n", ((dst / CHAR_BIT)  - 1));
+	}
+	print_bits(byte);
+	printf("\nvalmod: %d\n", valmod);
+	printf("bit value: %d\n", !!((byte << (valmod) & 0x80)));
+}
+
 void	set_link(t_mat_graph *graph, unsigned int src, unsigned int dst, char
 oriented)
 {
 	unsigned char valmod;
 
 	valmod = (dst % CHAR_BIT);
-	printf("dd: %d\n", (dst / CHAR_BIT));
 	if (valmod)
 		graph->matrix[src][dst / CHAR_BIT] |= 1 << (CHAR_BIT - (valmod));
-	else if ((dst / CHAR_BIT) == 0)
-		graph->matrix[src][(dst / CHAR_BIT)] |= 1 << CHAR_BIT - 1;
 	else
-		graph->matrix[src][((dst / CHAR_BIT) - 1)] |= 1;
+		graph->matrix[src][(dst / CHAR_BIT)] |= (1 << (CHAR_BIT - 1));
+//	else
+//		graph->matrix[src][((dst / CHAR_BIT) - 1)] |= 1;
 	if (!oriented)
 		set_link(graph, dst, src, 1);
 }
+
+void	unset_link(t_mat_graph *graph, unsigned int src, unsigned int dst, char
+oriented)
+{
+	unsigned char valmod;
+
+	valmod = (dst % CHAR_BIT);
+	if (valmod)
+		graph->matrix[src][dst / CHAR_BIT] &= ~(1 << (CHAR_BIT - (valmod)));
+	else if ((dst / CHAR_BIT) == 0)
+		graph->matrix[src][(dst / CHAR_BIT)] &= ~(1 << (CHAR_BIT - 1));
+	else
+		graph->matrix[src][((dst / CHAR_BIT) - 1)] &= ~(1);
+	if (!oriented)
+		set_link(graph, dst, src, 1);
+}
+
 
 t_mat_graph *new_mat_graph(unsigned long size)
 {
@@ -138,23 +182,75 @@ t_mat_graph *new_mat_graph(unsigned long size)
     return (graph);
 }
 
+//int minDistance(t_mat_graph *graph, int dist[], char sptSet[])
+//{
+//	int min = INT_MAX;
+//	int min_index;
+//
+//	for (int v = 0; v < graph->size; v++)
+//	{
+//		if (sptSet[v] == 0 && dist[v] <= min)
+//		{
+//			min = dist[v];
+//			min_index = v;
+//		}
+//	}
+//	return (min_index);
+//}
+
+//void dijkstra(t_mat_graph *graph, unsigned int src, unsigned int dst)
+//{
+//	int		dist[graph->size];
+//	int		u;
+//	char	sptSet[graph->size];
+//
+//	for (int i = 0; i < graph->size; i++)
+//	{
+//		dist[i] = INT_MAX;
+//		sptSet[i] = 0;
+//	}
+//	dist[src] = 0;
+//	for (int count = 0; count < (graph->size - 1); count++)
+//	{
+//		u = minDistance(graph, dist, sptSet);
+//		sptSet[u] = 1;
+//		for (int v = 0; v < graph->size; v++)
+//		{
+//			if (sptSet[v] == 0 && )
+//		}
+//	}
+//}
+
 int main() {
 	t_mat_graph *graph;
 
-	graph = new_mat_graph(10); 
+	graph = new_mat_graph(16);
 //	set_link(graph, 1, 2, 1);
 	set_link(graph, 8, 0, 1);
-//	set_link(graph, 7, 1, 1);
+	set_link(graph, 8, 1, 1);
 //	set_link(graph, 7, 2, 1);
 //	set_link(graph, 7, 3, 1);
 //	set_link(graph, 7, 4, 1);
 //	set_link(graph, 7, 5, 1);
 //	set_link(graph, 7, 6, 1);
+//	set_link(graph, 8, 7, 1);
 	set_link(graph, 8, 7, 1);
 	set_link(graph, 8, 8, 1);
 //	set_link(graph, 8, 9, 1);
 	set_link(graph, 8, 15, 1);
 	set_link(graph, 8, 16, 1);
+	printf("8-0 %d\n\n", has_link(graph, 8, 0));
+	printf("8-1 %d\n\n", has_link(graph, 8, 1));
+	printf("8-2 %d\n\n", has_link(graph, 8, 2));
+	printf("8-3 %d\n\n", has_link(graph, 8, 3));
+	printf("8-4 %d\n\n", has_link(graph, 8, 4));
+	printf("8-5 %d\n\n", has_link(graph, 8, 5));
+	printf("8-6 %d\n\n", has_link(graph, 8, 6));
+	printf("8-7 %d\n\n", has_link(graph, 8, 7));
+	printf("8-8 %d\n\n", has_link(graph, 8, 8));
+	printf("8-9 %d\n\n", has_link(graph, 8, 9));
+	printf("8-15 %d\n\n", has_link(graph, 8, 15));
+	printf("8-16 %d\n\n", has_link(graph, 8, 16));
 //	set_link(graph, 7, 9, 1);
 //	set_link(graph, 3, 1, 1);
 	print_matrix(graph);
