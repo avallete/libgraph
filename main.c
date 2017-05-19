6c81900276912ede12d2aab624856923889dcc12
Andrew Valleteau <avallete@student.42.fr>
2017-05-19 06:37:48 +0200

    [FEAT] Add Djikstra

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include "libft/includes/libft.h"
 
 typedef struct      s_mat_graph
 {
-    unsigned long   size;
+    unsigned long size;
     char            **matrix;
 }                   t_mat_graph;
 
@@ -42,33 +43,19 @@ void	ft_bzero(void *s, size_t n)
 
 static void        init_matrix(t_mat_graph *graph)
 {
-    unsigned long   rowlen;
     unsigned long   i;
 
     i = 0;
-    rowlen = ((graph->size / CHAR_BIT) + 1);
     while (i < graph->size)
     {
-        if (!(graph->matrix[i] = (char*)malloc(sizeof(char) * (rowlen + 1))))
+        if (!(graph->matrix[i] = (char*)malloc(sizeof(char) * graph->size)))
         {
             del_mat_graph(graph);
             break;
         }
-        ft_bzero(graph->matrix[i], rowlen);
+        ft_bzero(graph->matrix[i], graph->size);
         ++i;
     }
-}
-
-void		print_bits(char b)
-{
-	char i;
-
-	i = 0;
-	while (i < CHAR_BIT)
-	{
-		printf("%d ", !!((b << i) & 0x80));
-		++i;
-	}
 }
 
 void		print_matrix(t_mat_graph *graph)
@@ -78,22 +65,22 @@ void		print_matrix(t_mat_graph *graph)
 	unsigned int e;
 
 	i = 0;
-	rowlen = ((graph->size / CHAR_BIT) + 1);
+	printf("Size: %lu\n", graph->size);
 	printf("--  ");
 	e = 0;
-	while (e < (rowlen * CHAR_BIT))
+	while (e < graph->size)
 	{
-		printf("%d ", (e % CHAR_BIT));
+		printf("%-5d", (e % 99999));
 		++e;
 	}
 	printf("\n");
 	while (i < graph->size)
 	{
 		e = 0;
-		printf("%d : ", i);
-		while (e < rowlen)
+		printf("%-5d", (i % 99999));
+		while (e < graph->size)
 		{
-			print_bits(graph->matrix[i][e]);
+			printf("%-5d", graph->matrix[i][e]);
 			++e;
 		}
 		printf("\n");
@@ -101,64 +88,33 @@ void		print_matrix(t_mat_graph *graph)
 	}
 }
 
-#define CHECK_BIT(var, pos) ((var) & (1 << (pos)))
-
-int has_link(t_mat_graph *graph, unsigned int src, unsigned int dst)
+char 	has_link(t_mat_graph *graph, unsigned int src, unsigned int dst)
 {
-	unsigned char valmod;
-	int byte;
-
-	valmod = (dst % CHAR_BIT);
-	if (valmod)
-	{
-		byte = graph->matrix[src][dst / CHAR_BIT];
-		printf("byteindex1: %d\n", (dst / CHAR_BIT));
-	}
-	else if ((dst / CHAR_BIT) == 0)
-	{
-		byte = graph->matrix[src][(dst / CHAR_BIT)];
-		printf("byteindex2: %d\n", ((dst / CHAR_BIT)));
-	}
-	else
-	{
-		byte = graph->matrix[src][((dst / CHAR_BIT) - 1)];
-		printf("byteindex3: %d\n", ((dst / CHAR_BIT)  - 1));
-	}
-	print_bits(byte);
-	printf("\nvalmod: %d\n", valmod);
-	printf("bit value: %d\n", !!((byte << (valmod) & 0x80)));
+	if (src < graph->size && dst < graph->size)
+		return (graph->matrix[src][dst]);
+	return (0);
 }
 
 void	set_link(t_mat_graph *graph, unsigned int src, unsigned int dst, char
 oriented)
 {
-	unsigned char valmod;
-
-	valmod = (dst % CHAR_BIT);
-	if (valmod)
-		graph->matrix[src][dst / CHAR_BIT] |= 1 << (CHAR_BIT - (valmod));
-	else
-		graph->matrix[src][(dst / CHAR_BIT)] |= (1 << (CHAR_BIT - 1));
-//	else
-//		graph->matrix[src][((dst / CHAR_BIT) - 1)] |= 1;
-	if (!oriented)
-		set_link(graph, dst, src, 1);
+	if (dst < graph->size && src < graph->size)
+	{
+		graph->matrix[src][dst] = 1;
+		if (!oriented)
+			set_link(graph, dst, src, 1);
+	}
 }
 
 void	unset_link(t_mat_graph *graph, unsigned int src, unsigned int dst, char
 oriented)
 {
-	unsigned char valmod;
-
-	valmod = (dst % CHAR_BIT);
-	if (valmod)
-		graph->matrix[src][dst / CHAR_BIT] &= ~(1 << (CHAR_BIT - (valmod)));
-	else if ((dst / CHAR_BIT) == 0)
-		graph->matrix[src][(dst / CHAR_BIT)] &= ~(1 << (CHAR_BIT - 1));
-	else
-		graph->matrix[src][((dst / CHAR_BIT) - 1)] &= ~(1);
-	if (!oriented)
-		set_link(graph, dst, src, 1);
+	if (dst < graph->size && src < graph->size)
+	{
+		graph->matrix[src][dst] = 0;
+		if (!oriented)
+			set_link(graph, dst, src, 1);
+	}
 }
 
 
@@ -182,78 +138,91 @@ t_mat_graph *new_mat_graph(unsigned long size)
     return (graph);
 }
 
-//int minDistance(t_mat_graph *graph, int dist[], char sptSet[])
-//{
-//	int min = INT_MAX;
-//	int min_index;
-//
-//	for (int v = 0; v < graph->size; v++)
-//	{
-//		if (sptSet[v] == 0 && dist[v] <= min)
-//		{
-//			min = dist[v];
-//			min_index = v;
-//		}
-//	}
-//	return (min_index);
-//}
+int 	min_distance(t_mat_graph *graph, int *dist, char *sptSet)
+{
+	int min = INT_MAX;
+	int min_index = 0;
 
-//void dijkstra(t_mat_graph *graph, unsigned int src, unsigned int dst)
-//{
-//	int		dist[graph->size];
-//	int		u;
-//	char	sptSet[graph->size];
-//
-//	for (int i = 0; i < graph->size; i++)
-//	{
-//		dist[i] = INT_MAX;
-//		sptSet[i] = 0;
-//	}
-//	dist[src] = 0;
-//	for (int count = 0; count < (graph->size - 1); count++)
-//	{
-//		u = minDistance(graph, dist, sptSet);
-//		sptSet[u] = 1;
-//		for (int v = 0; v < graph->size; v++)
-//		{
-//			if (sptSet[v] == 0 && )
-//		}
-//	}
-//}
+	for (int v = 0; v < graph->size; v++)
+	{
+		if (sptSet[v] == 0 && dist[v] <= min)
+		{
+			min = dist[v];
+			min_index = v;
+		}
+	}
+	return (min_index);
+}
+
+void printPath(int *parent, int j)
+{
+	if (parent[j]==-1)
+		return;
+
+	printPath(parent, parent[j]);
+	printf("%d ", j);
+}
+
+int printSolution(int *dist, int src, int dst, int n, int *parent)
+{
+	printf("Vertex\t  Distance\tPath");
+	printf("\n%d -> %d :: ", src, dst);
+	printPath(parent, dst);
+}
+
+void 	dijkstra(t_mat_graph *graph, unsigned int src, unsigned int dst)
+{
+	int 	u;
+	int		dist[graph->size];
+	int 	parent[graph->size];
+	char	sptSet[graph->size];
+
+	for (int i = 0; i < graph->size; i++)
+	{
+		parent[0] = -1;
+		dist[i] = INT_MAX;
+		sptSet[i] = 0;
+	}
+	dist[src] = 0;
+	for (int count = 0; count < (graph->size - 1); count++)
+	{
+		u = min_distance(graph, dist, sptSet);
+		sptSet[u] = 1;
+
+		for (int v = 0; v < graph->size; v++)
+		{
+			if (sptSet[v] == 0 && graph->matrix[u][v] && dist[u] != INT_MAX
+				&& dist[u] + graph->matrix[u][v] < dist[v])
+			{
+				parent[v] = u;
+				dist[v] = dist[u] + graph->matrix[u][v];
+			}
+			if (u == dst)
+			{
+				printSolution(dist, src, dst, (int) graph->size, parent);
+				return ;
+			}
+		}
+	}
+	printf("No path from %d to %d", src, dst);
+}
 
 int main() {
 	t_mat_graph *graph;
 
-	graph = new_mat_graph(16);
-//	set_link(graph, 1, 2, 1);
-	set_link(graph, 8, 0, 1);
-	set_link(graph, 8, 1, 1);
-//	set_link(graph, 7, 2, 1);
-//	set_link(graph, 7, 3, 1);
-//	set_link(graph, 7, 4, 1);
-//	set_link(graph, 7, 5, 1);
-//	set_link(graph, 7, 6, 1);
-//	set_link(graph, 8, 7, 1);
-	set_link(graph, 8, 7, 1);
-	set_link(graph, 8, 8, 1);
-//	set_link(graph, 8, 9, 1);
-	set_link(graph, 8, 15, 1);
-	set_link(graph, 8, 16, 1);
-	printf("8-0 %d\n\n", has_link(graph, 8, 0));
-	printf("8-1 %d\n\n", has_link(graph, 8, 1));
-	printf("8-2 %d\n\n", has_link(graph, 8, 2));
-	printf("8-3 %d\n\n", has_link(graph, 8, 3));
-	printf("8-4 %d\n\n", has_link(graph, 8, 4));
-	printf("8-5 %d\n\n", has_link(graph, 8, 5));
-	printf("8-6 %d\n\n", has_link(graph, 8, 6));
-	printf("8-7 %d\n\n", has_link(graph, 8, 7));
-	printf("8-8 %d\n\n", has_link(graph, 8, 8));
-	printf("8-9 %d\n\n", has_link(graph, 8, 9));
-	printf("8-15 %d\n\n", has_link(graph, 8, 15));
-	printf("8-16 %d\n\n", has_link(graph, 8, 16));
-//	set_link(graph, 7, 9, 1);
-//	set_link(graph, 3, 1, 1);
+	graph = new_mat_graph(10);
+	set_link(graph, 0, 3, 1);
+	set_link(graph, 0, 1, 1);
+	set_link(graph, 1, 2, 1);
+	set_link(graph, 2, 4, 1);
+	set_link(graph, 3, 4, 1);
+	set_link(graph, 3, 6, 1);
+	set_link(graph, 4, 5, 1);
+	set_link(graph, 6, 4, 1);
+	set_link(graph, 6, 7, 1);
+	set_link(graph, 7, 5, 1);
 	print_matrix(graph);
+	dijkstra(graph, 0, 5);
 	del_mat_graph(graph);
     return 0;
 }
